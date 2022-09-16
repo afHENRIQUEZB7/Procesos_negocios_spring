afb355932f4e2317c492a29b50c23651c2b26c7b
afHENRIQUEZB7 <afhenriquezb@ufpso.edu.co>
2022-09-15 19:39:33 -0500

    commit inicila del proyecto

---
+++ src/test/java/com/Procesos/negocios/AFHenr/quezB/ProcesosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Procesos.negocios.AFHenr.quezB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
